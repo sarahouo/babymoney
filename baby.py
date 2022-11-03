77e95f6944bb53554f1556bec2b55e15c4bffeb5
sarahouo <117375006+sarahouo@users.noreply.github.com>
2022-11-04 00:52:24 +0800

    Update baby.py

--- baby.py
+++ baby.py
@@ -1,52 +1,29 @@
-from flask import Flask, request, abort
+from flask_ngrok import run_with_ngrok
+from flask import Flask, request
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import TextSendMessage   # 載入 TextSendMessage 模組
+import json
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
+app = Flask(__name__)
 
-baby = Flask(__name__)
-
-# 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('hkUCqCIH7OadeaYR92b/j7g1m/rtSlhUp0HRV2hwXxY2Z5wnmsmI39RVr2InQLzujiS1ozkrNSoV7bu9kAJ3JxukEJLaFZwRE0Sg1dfM/yCz/XINVjzJnBodPgQWFGfzLFXM5XolxBbUMcTVbB8P9QdB04t89/1O/w1cDnyilFU=')
-# 必須放上自己的Channel Secret
-handler = WebhookHandler('b41edbbd4896962a21919048f27e5c82')
-
-line_bot_api.push_message('U75b15c083783a40fd1f2a6dc1f9b56db', TextSendMessage(text='你可以開始了'))
-
-
-# 監聽所有來自 /callback 的 Post Request
-@baby.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
- 
-    # get request body as text
+@app.route("/", methods=['POST'])
+def linebot():
     body = request.get_data(as_text=True)
-    baby.logger.info("Request body: " + body)
-
-    # handle webhook body
+    json_data = json.loads(body)
+    print(json_data)
     try:
+        line_bot_api = LineBotApi('hkUCqCIH7OadeaYR92b/j7g1m/rtSlhUp0HRV2hwXxY2Z5wnmsmI39RVr2InQLzujiS1ozkrNSoV7bu9kAJ3JxukEJLaFZwRE0Sg1dfM/yCz/XINVjzJnBodPgQWFGfzLFXM5XolxBbUMcTVbB8P9QdB04t89/1O/w1cDnyilFU=')
+        handler = WebhookHandler('b41edbbd4896962a21919048f27e5c82')
+        signature = request.headers['X-Line-Signature']
         handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
+        tk = json_data['events'][0]['replyToken']         # 取得 reply token
+        msg = json_data['events'][0]['message']['text']   # 取得使用者發送的訊息
+        text_message = TextSendMessage(text=msg)          # 設定回傳同樣的訊息
+        line_bot_api.reply_message(tk,text_message)       # 回傳訊息
+    except:
+        print('error')
     return 'OK'
 
- 
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
-
-#主程式
-import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    baby.run(host='0.0.0.0', port=port)
+    run_with_ngrok(app)
+    app.run()
