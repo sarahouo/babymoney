0406772ddd4e2b046632fd34618beb94b1d6720f
sarahouo <117375006+sarahouo@users.noreply.github.com>
2022-11-04 00:42:06 +0800

    Update baby.py

--- baby.py
+++ baby.py
@@ -48,5 +48,5 @@ def handle_message(event):
 #主程式
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     baby.run(host='0.0.0.0', port=port)
